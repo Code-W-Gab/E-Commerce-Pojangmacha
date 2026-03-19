2e177a90f9267ba81808abe2db5a6329a1fb8bea
Code-W-Gab <gabconcepcion02@gmail.com>
2026-03-19 12:36:52 +0800

    Make the Header Nav functional

--- Frontend/src/components/common/Header.jsx
+++ Frontend/src/components/common/Header.jsx
@@ -1,13 +1,14 @@
 import {ShoppingBasket} from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return(
     <header className='flex items-center justify-between'>
       <h1 className='text-2xl font-bold text-orange-500'>Pojangmacha</h1>
       <div className='flex items-center gap-6 text-lg text-gray-500'>
-        <p>home</p>
-        <p>menu</p>
-        <p>contact us</p>
+        <Link to={"/Home"}>home</Link>
+        <Link to={"/Menu"}>menu</Link>
+        <Link to={"/Contacts"}>contact us</Link>
       </div>
       <div className='flex items-center gap-6'>
         <ShoppingBasket size={30}/>
