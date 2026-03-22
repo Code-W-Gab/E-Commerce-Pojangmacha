51dc50de4a0d1e5f0db649ae056eaad830c08598
Code-W-Gab <gabconcepcion02@gmail.com>
2026-03-22 18:25:45 +0800

    Add header to Contact  Page

--- Frontend/src/pages/ContactPage.jsx
+++ Frontend/src/pages/ContactPage.jsx
@@ -1,7 +1,10 @@
+import Header from "../components/common/Header";
+
 export default function ContactPage() {
   return(
-    <div>
-      Contact Page
-    </div>
+    <main className="py-4 px-50">
+      <Header/>
+      <div>Contact Page</div>
+    </main>
   )
 }
